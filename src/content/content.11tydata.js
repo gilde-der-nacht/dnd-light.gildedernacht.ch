1a3433ee8ecd8bc99a00a05de705aba9c5d02b69
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-03-26 14:12:06 +0000

    overwrite 11ty clean url output

--- src/content/content.11tydata.js
+++ src/content/content.11tydata.js
@@ -1,3 +1,10 @@
 module.exports = {
-  layout: "base.njk"
+  layout: "base.njk",
+  eleventyComputed: {
+    permalink: ({ page }) => {
+      const re = new RegExp("^.+(?=" + page.fileSlug + "$)");
+      const path = re.exec(page.filePathStem);
+      return `${path ?? ""}${page.fileSlug ?? "index"}.html`;
+    }
+  }
 }
